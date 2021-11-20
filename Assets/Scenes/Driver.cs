9e8570fc348f61e3b2746c87ca114081e96932ee
davelush <davelush@github.com>
2021-11-20 09:56:11 +0000

    Rotating slowly around a center point

--- Assets/Scenes/Driver.cs
+++ Assets/Scenes/Driver.cs
@@ -7,12 +7,15 @@ public class Driver : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // transform.Rotate(0,0,45);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // transform.Rotate(0,0,1);
+        // transform.Translate(Vector3.up * Time.deltaTime);
+        transform.Rotate(0,0,0.1f);
+        transform.Translate(0, 0.01f, 0);
     }
 }
